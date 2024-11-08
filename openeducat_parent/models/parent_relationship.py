1c421ebddc2b611a5d68f59a66e2b2cbe7f5d4ff
udaybhola7851 <uday@openeducat.com>
2024-11-08 11:24:40 +0530

    [17.0][ADD] Added relationship name constraint

--- openeducat_parent/models/parent_relationship.py
+++ openeducat_parent/models/parent_relationship.py
@@ -27,3 +27,9 @@ class OpParentRelation(models.Model):
     _description = "Relationships"
 
     name = fields.Char('Name', required=True)
+
+    _sql_constraints = [(
+        'unique_relationship_name',
+        'unique(name)',
+        'Can not create relationship multiple times.!'
+    )]
